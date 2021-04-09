3e13e132ddcd2f03a8b2664d6f1d1a3ac04fd236
Sameer Khan <samsam23122001@gmail.com>
2021-04-09 15:22:20 -0400

    Health bar, collision tracking, and game over screen

--- game.asm
+++ game.asm
@@ -42,6 +42,8 @@
 .eqv 	BLACK		0x00000000
 .eqv	SHIP_SIZE	40
 .eqv	OBSTACLE_SIZE	32
+.eqv	HEALTH_ADDRESS	0x10009E08
+.eqv	HEALTH_SIZE	5
 
 .data
 W:	.asciiz		"w"
@@ -51,7 +53,7 @@ D:	.asciiz		"d"
 SHIP_ARRAY:    		.word    8, 12, 256, 260, 272, 512, 516, 528, 776, 780
 OBSTACLE_ARRAY:    	.word    4, 8, 256, 268, 512, 524, 772, 776
 exit:	.asciiz		"Exiting Game!"
-collision:	.asciiz	"Collision!"
+restart:	.asciiz	"Restarting!"
 .text
 .globl SETUP
 
@@ -98,6 +100,7 @@ SETUP: 	li $t0, BASE_ADDRESS	# $t0 stores the base address for display
 	# Counter variables
 	add $s6, $s6, $zero	# Update spawn time
 	add $s0, $s0, $zero	# Delay collision
+	add $s7, $s7, $zero	# Collision count
 	
 	j SHIP
 	
@@ -107,7 +110,7 @@ MAIN:	lw $t8, 0($t9)
 	
 	j SHIP
 	
-KEY:	lw $t2, 4($t9) # this assumes $t9 is set to 0xfff0000from before
+KEY:	lw $t2, 4($t9) # this assumes $t9 is set to 0xfff0000 from before
 	beq $t2, 0x77, W_PRESS # ASCII code of 'w' is 0x77
 	beq $t2, 0x61, A_PRESS # ASCII code of 'a' is 0x61
 	beq $t2, 0x73, S_PRESS # ASCII code of 's' is 0x73
@@ -247,11 +250,11 @@ CALL_COLLISION:
 	
 	# Call Function
 	jal COLLISION_LOOP
-	j END_LOOP
+	j HEALTHBAR
 
 STAGGER: 
 	addi $s6, $s6, 1
-	j END_LOOP
+	j HEALTHBAR
 
 #=========================
 # OBSTACLE FUNCTION
@@ -341,14 +344,12 @@ COLLISION_LOOP:
 	
 SHOW_COLLISION:
 	bgt $s0, 0, WAIT_COL
-
-	li $v0, 4
-	la $a0, collision
-	syscall
+	addi $s7, $s7, 1	# Add weighted collision
 	
-	addi $s0, $s0, 1
+	addi $s0, $s0, 1	# Add delay for collision
 	j END_COL
 
+# Delay collision
 WAIT_COL:
 	addi $s0, $s0, 1
 	bgt $s0, 4, RESET_COL
@@ -361,16 +362,250 @@ END_COL:
 	jr $ra
 
 #=========================
+# HEALTHBAR
+#=========================
+HEALTHBAR:
+	# Load in parameters
+	li $t1, 0x5dcbc0	# $t1 stores the green
+	li $t3, 0
+	li $t4, HEALTH_ADDRESS
+	li $t5, HEALTH_SIZE
+	sub $t5, $t5, $s7
+	
+	# Game over screen
+	beq $t5, 0, GAME_OVER
+	# If game not over show health remaining
+	jal SHOWBAR
+	
+	# Show health bar
+	li $t3, 0
+	li $t1, 0xfa4454
+	jal RESET_BAR
+	
+	j END_LOOP
+
+SHOWBAR:
+	# Show green colour for health remaining
+	sw $t1, 0($t4)
+	addi $t4, $t4, 4
+	addi $t3, $t3, 1
+	bne $t3, $t5, SHOWBAR
+	jr $ra
+
+RESET_BAR:
+	# Show red for missing health
+	beq $t3, $s7, END_BAR
+	sw $t1, 0($t4)
+	addi $t4, $t4, 4
+	addi $t3, $t3, 1
+	bne $t3, $s7, RESET_BAR
+
+END_BAR:
+	jr $ra
+
+#=========================
+# GAME OVER
+#=========================
+GAME_OVER:
+	li $t1, BASE_ADDRESS
+	li $t2, END
+	li $t3, 0xfcdb03
+	
+SCREEN_RESET:
+	sw $t3, 0($t1)
+	addi $t1, $t1, 4
+	bne $t1, $t2, SCREEN_RESET
+	
+GAME_OVER_SCREEN:
+	li $t1, BASE_ADDRESS
+	li $t2, 0x000000
+	
+	# G LETTER
+	sw $t2, 260($t1)
+	sw $t2, 264($t1)
+	sw $t2, 268($t1)
+	sw $t2, 272($t1)
+	
+	sw $t2, 516($t1)
+	
+	sw $t2, 772($t1)
+	sw $t2, 780($t1)
+	sw $t2, 784($t1)
+	
+	sw $t2, 1028($t1)
+	sw $t2, 1040($t1)
+	
+	sw $t2, 1284($t1)
+	sw $t2, 1288($t1)
+	sw $t2, 1292($t1)
+	sw $t2, 1296($t1)
+	
+	# A LETTER
+	sw $t2, 280($t1)
+	sw $t2, 284($t1)
+	sw $t2, 288($t1)
+	sw $t2, 292($t1)
+	
+	sw $t2, 536($t1)
+	sw $t2, 548($t1)
+	
+	sw $t2, 792($t1)
+	sw $t2, 804($t1)
+	
+	sw $t2, 1048($t1)
+	sw $t2, 1052($t1)
+	sw $t2, 1056($t1)
+	sw $t2, 1060($t1)
+	
+	sw $t2, 1304($t1)
+	sw $t2, 1316($t1)
+	
+	# M LETTER
+	sw $t2, 300($t1)
+	sw $t2, 316($t1)
+	
+	sw $t2, 556($t1)
+	sw $t2, 560($t1)
+	sw $t2, 568($t1)
+	sw $t2, 572($t1)
+	
+	sw $t2, 812($t1)
+	sw $t2, 820($t1)
+	sw $t2, 828($t1)
+	
+	sw $t2, 1068($t1)
+	sw $t2, 1084($t1)
+	
+	sw $t2, 1324($t1)
+	sw $t2, 1340($t1)
+	
+	# E LETTER
+	sw $t2, 324($t1)
+	sw $t2, 328($t1)
+	sw $t2, 332($t1)
+	sw $t2, 336($t1)
+	
+	sw $t2, 580($t1)
+	
+	sw $t2, 836($t1)
+	sw $t2, 840($t1)
+	sw $t2, 844($t1)
+	
+	sw $t2, 1092($t1)
+	
+	sw $t2, 1348($t1)
+	sw $t2, 1352($t1)
+	sw $t2, 1356($t1)
+	sw $t2, 1360($t1)
+	
+	# O LETTER
+	sw $t2, 6836($t1)
+	sw $t2, 6840($t1)
+	
+	sw $t2, 7088($t1)
+	sw $t2, 7100($t1)
+	
+	sw $t2, 7344($t1)
+	sw $t2, 7356($t1)
+	
+	sw $t2, 7600($t1)
+	sw $t2, 7612($t1)
+	
+	sw $t2, 7860($t1)
+	sw $t2, 7864($t1)
+	
+	# V LETTER
+	sw $t2, 6852($t1)
+	sw $t2, 6864($t1)
+	
+	sw $t2, 7108($t1)
+	sw $t2, 7120($t1)
+	
+	sw $t2, 7364($t1)
+	sw $t2, 7376($t1)
+	
+	sw $t2, 7620($t1)
+	sw $t2, 7632($t1)
+	
+	sw $t2, 7880($t1)
+	sw $t2, 7884($t1)
+	
+	# E LETTER 
+	sw $t2, 6872($t1)
+	sw $t2, 6876($t1)
+	sw $t2, 6880($t1)
+	sw $t2, 6884($t1)
+	
+	sw $t2, 7128($t1)
+	
+	sw $t2, 7384($t1)
+	sw $t2, 7388($t1)
+	sw $t2, 7392($t1)
+	
+	sw $t2, 7640($t1)
+	
+	sw $t2, 7896($t1)
+	sw $t2, 7900($t1)
+	sw $t2, 7904($t1)
+	sw $t2, 7908($t1)
+	
+	# R LETTER
+	sw $t2, 6892($t1)
+	sw $t2, 6896($t1)
+	sw $t2, 6900($t1)
+	sw $t2, 6904($t1)
+	
+	sw $t2, 7148($t1)
+	sw $t2, 7160($t1)
+	
+	sw $t2, 7404($t1)
+	sw $t2, 7416($t1)
+	
+	sw $t2, 7660($t1)
+	sw $t2, 7664($t1)
+	sw $t2, 7668($t1)
+	
+	sw $t2, 7916($t1)
+	sw $t2, 7928($t1)
+	
+	j RESTART
+
+#=========================
+# RESTART GAME
+#=========================
+RESTART:
+	lw $t8, 0($t9)
+	beq $t8, 1, P_KEY
+	
+	j END_RESTART
+	
+P_KEY:	lw $t2, 4($t9) # this assumes $t9 is set to 0xfff0000 from before
+	beq $t2, 0x70, P_PRESS # ASCII code of 'P' is 0X70
+	beq $t2, 0x78, EXIT
+
+P_PRESS:
+	li $v0, 4
+	la $a0, restart
+	syscall
+	j SETUP
+
+END_RESTART:
+	# SLEEP 20 MILLISECOND
+	li $v0, 32
+	li $a0, REFRESH_RATE  # Wait REFRESH_RATE amount of seconds
+	syscall
+	j RESTART
+	
+#=========================
 # END LOOP AND RESET
 #=========================
 END_LOOP:
-
-	# SLEEP 1 SECOND
+	# SLEEP 20 MILLISECOND
 	li $v0, 32
 	li $a0, REFRESH_RATE  # Wait REFRESH_RATE amount of seconds
 	syscall
 	j MAIN
-	
+
 EXIT:	# Print Exit String
 	li $v0, 4
 	la $a0, exit
